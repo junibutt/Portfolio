6530a2d8eba6700115302fcc9da66396019e4131
Muhammad Junaid <junnedbutt@gmail.com>
2025-05-16 04:49:54 -0700

    updating

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,7 +3,7 @@ import img2 from "../assets/2.png";
 import img3 from "../assets/3.png";
 
 import img5 from "../assets/5.png";
-import chatbot from "../assets/chatbot.jpg"
+import chatbot from "../assets/6.png"
 import { motion } from "framer-motion";
 
 const projectsData = [
@@ -14,29 +14,25 @@ const projectsData = [
     linkk: "https://juni-venture.vercel.app/"
   },
   {
-    image: img5,
-    title: "Monks Estate",
-    description: "Monks Estate showcases a sleek design offering real estate services like buying, renting, and property management, with an emphasis on client satisfaction, awards, and personalized property discovery.",
-    linkk: "https://monks-estate.vercel.app/#blog"
-  },
-  {
     image: img2,
     title: "Tutor Hub",
     description: "This website offers flexible, online language learning with expert tutors, personalized lessons, and affordable prices, providing a seamless experience for mastering new languages anytime, anywhere. {Email:user@example.com , Password:user123 }",
     linkk: "https://tutor-hub-taupe.vercel.app/#"
   },
+ 
   {
+    image: chatbot,
+    title: "SafeLoan",
+    description: "SafeLoan is a responsive React-based web app that simplifies loan applications, showcasing loan options, eligibility, FAQs, and contact info with clean UI and smooth UX.",
+    linkk: "https://safe-loan-git-main-muhammad-junaids-projects-545d53e5.vercel.app/"
+  } ,
+   {
     image: img3,
     title: "Juni Bazaar",
     description: "Junibazar offers a  dynamic e-commerce platform with free shipping, easy returns, and a wide variety of products, from fashion to electronics, prioritizing customer satisfaction.",
     linkk: "https://juni-bazaar-ecommerce-store.vercel.app/"
   },
-  {
-    image: chatbot,
-    title: "AI Chatbot",
-    description: "The AI chatbot offers interactive conversations, providing intelligent, context-aware responses. It adapts to various queries, assisting with information, tasks, and engaging discussions for users.",
-    linkk: "https://ai-bot-git-main-muhammad-junaids-projects-545d53e5.vercel.app/"
-  }
+
 ];
 
 const ScrollReveal = ({ children }) => {
